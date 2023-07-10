ab88cf934149576c9cc74a0fa95faafcf8beeed6
leidyz <leidy.zhang98@gmail.com>
2023-07-10 12:16:56 +0200

    version3

---
+++ Level1 .php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> Embedded PHP</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+            $a = 18;
+            $b = 5;
+            echo "Hello, world! Let's add. \n"; //echo means to print
+            echo "the sum of $a + $b = " . ($a + $b); //the defined variables can be inside the quotations
+           
+            if($a > $b){  //added the new conditional statement in the third version
+                echo "$a is greater than $b";
+            }else{
+                echo "$a is less than $b";
+            }
+            ?>
+        </h1>
+    </body>
+</html>
